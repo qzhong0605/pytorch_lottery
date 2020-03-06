b65f9270e69a42d5de4a15a6dc650b44b89adee0
qzhong0605 <qzhong2013@gmail.com>
2020-03-06 23:09:19 +0800

    fix bug on lenet

--- models/mnist/lenet.py
+++ models/mnist/lenet.py
@@ -4,29 +4,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LeNet(Module):
-    def __init__(self):
+    def __init__(self, num_classes=10):
         super(LeNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout2d(0.25)
-        self.dropout2 = nn.Dropout2d(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=1, stride=2),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(64*14*14, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, num_classes)
+        )
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-
-        x = self.conv2(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
+        x = self.features(x)
         x = torch.flatten(x, 1)
-
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-
+        x = self.classifier(x)
         output = F.log_softmax(x)
         return output
 
