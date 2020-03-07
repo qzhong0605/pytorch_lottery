8f01f3f99d0ad2b7a152b96da23577a5f5c1b120
qzhong0605 <qzhong2013@gmail.com>
2020-03-08 00:08:11 +0800

    copy ipdb files from ipdb project

---
+++ ipdb/__init__.py
@@ -0,0 +1,7 @@
+# It's based on ipdb project
+
+from ipdb.__main__ import set_trace, post_mortem, pm, run             # noqa
+from ipdb.__main__ import runcall, runeval, launch_ipdb_on_exception  # noqa
+
+from ipdb.stdout import sset_trace, spost_mortem, spm                 # noqa
+from ipdb.stdout import slaunch_ipdb_on_exception                     # noqa
