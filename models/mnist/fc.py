3887517847718ac30fecaba6cbfb830701d2ee20
qzhong0605 <qzhong2013@gmail.com>
2020-03-07 20:53:40 +0800

    refactor fc network for mnist dataset

--- models/mnist/fc.py
+++ models/mnist/fc.py
@@ -6,21 +6,18 @@ from models import base_model
 class FC(base_model.HookModule):
     def __init__(self, device):
         super(FC, self).__init__(device)
-        self.fc1 = nn.Linear(28*28, 300)
-        self.fc2 = nn.Linear(300, 100)
-        self.fc3 = nn.Linear(100, 10)
+        self.features = nn.Sequential(
+            nn.Flatten(start_dim=1),
+            nn.Linear(28*28, 300),
+            nn.ReLU(),
+            nn.Linear(300, 100),
+            nn.ReLU(),
+            nn.Linear(100, 10)
+        )
 
-    def forward(self,x ):
-        x = torch.flatten(x, 1)
-        x = self.fc1(x);
-        x = F.relu(x, inplace=True)
-
-        x = self.fc2(x)
-        x = F.relu(x, inplace=True)
-
-        x = self.fc3(x)
-
-        output = F.log_softmax(x)
+    def forward(self,x):
+        out = self.features(x)
+        output = F.log_softmax(out)
         return output
 
 
