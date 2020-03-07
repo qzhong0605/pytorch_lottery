9ed440a525585d3b11bfa2a491f21daaa017d478
qzhong0605 <qzhong2013@gmail.com>
2020-03-07 17:38:56 +0800

    add sparsity function for model hook

--- utils.py
+++ utils.py
@@ -1,13 +1,20 @@
 import torch
 
 def show_details_of_model(model):
-    """ It's used to display the details of the network, shows the weights and
+    """ It's used to display the details of the network, including the weights and
     shapes
     """
     print('=========================== weight details of network ===============================')
     for name, param in model.named_parameters():
         print(f'{name}: \t {param.shape}')
     print('=====================================================================================')
+
+
+def show_sparsity_of_model(model):
+    """ It's used to display the sparsity of the network, including the weight shape
+    and number of non-zero weights
+    """
+    pass
 
 
 def setup_dataloader(batch_size):
