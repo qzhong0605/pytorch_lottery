5d8766490e6688a100b52b4f6e9d1b8335f5921d
qzhong0605 <qzhong2013@gmail.com>
2020-03-07 21:12:49 +0800

    add abs lottery module

--- lottery_modules/elements.py
+++ lottery_modules/elements.py
@@ -7,7 +7,6 @@ class Sum(nn.Module):
 
     The inputs must be the same shape. And the out shape is also the same with the input tensor
     """
-
     def __init__(self):
         super(Sum, self).__init__()
 
@@ -15,3 +14,15 @@ class Sum(nn.Module):
         """ The input must be a list with length of 2
         """
         return input[0] + input[1]
+
+
+class Abs(nn.Module):
+    r"""
+    Perform abs operation on each element of the input tensor.
+    It returns an output tensor whose shape is the same with the input tensor
+    """
+    def __init__(self):
+        super(Abs, self).__init__()
+
+    def forward(self, input):
+        return torch.abs(input)
