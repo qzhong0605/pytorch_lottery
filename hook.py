49e71bd7b87344f9c04bd420d8aca1ced2df06fc
qzhong0605 <qzhong2013@gmail.com>
2020-04-18 10:29:12 +0800

    add count_zeros, breakpoint_on_module and percentile function on utils

--- hook.py
+++ hook.py
@@ -53,5 +53,8 @@ def module_debug(module, input, output):
     from utils import backtrace_modules
     from utils import retrieve_module
     from utils import clear_breakpoint
+    from utils import breakpoint_on_module
+    from utils import count_zeros
+    from utils import percentile
 
     import module_ipdb; module_ipdb.set_trace()
