ffc928fa7c8d61ad603062ab873615c2c11dd8d4
qzhong0605 <qzhong2013@gmail.com>
2020-07-09 19:26:06 +0800

    add resnet network to train on imagenet dataset

--- models/imagenet/__init__.py
+++ models/imagenet/__init__.py
@@ -1,4 +1,5 @@
 from models.imagenet import vgg
+from models.imagenet import resnet
 
 # a list of models for tiny imagenet dataset
 
@@ -11,4 +12,14 @@ __model__ = {
     'vgg16bn' : vgg.build_vgg16bn,
     'vgg19' : vgg.build_vgg19,
     'vgg19bn' : vgg.build_vgg19bn,
+
+    'resnet18' : resnet.build_resnet18,
+    'resnet34' : resnet.build_resnet34,
+    'resnet50' : resnet.build_resnet50,
+    'resnet101' : resnet.build_resnet101,
+    'resnet152' : resnet.build_resnet152,
+    'resnext50_32x4d' : resnet.build_resnext50_32x4d,
+    'resnext101_32x8d' : resnet.build_resnext101_32x8d,
+    'wide_resnet50_2' : resnet.build_wide_resnet50_2,
+    'build_wide_resnet101_2' : resnet.build_wide_resnet101_2,
 }
